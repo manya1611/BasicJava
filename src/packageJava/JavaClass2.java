858abecb06f4636708734835dca24b38a636c4bb
Admin <Admin@192.168.2.8>
2022-06-01 17:08:21 +0530

    Now The For Java

---
+++ src/packageJava/JavaClass2.java
@@ -0,0 +1,100 @@
+package packageJava;
+
+public class JavaClass2 {
+	public static void main(String[] args)
+	{
+		// program 1
+		int a=87;
+		int e=98;
+		int f= a + e;
+		System.out.println(f);
+	/*	
+	//	program 2
+		
+		//Student-Data
+		//StudentName,Id,Class,Mobile,Percentage
+		
+		String studentName = "Pratik Jadhav";
+		int studentId = 7387;
+		char studentDivision = 'A';
+		int StudentNumber = 83909708;
+		
+		double StudentPercentage = 72;
+		
+		System.out.println(studentName);
+		System.out.println(studentId);
+		System.out.println(studentDivision);
+		System.out.println(StudentNumber);
+		System.out.println(StudentPercentage);
+		
+		*/
+		//program 3
+		
+		String StudentName ="Rasika";
+		
+		System.out.println("Student Name is " +   StudentName);
+		
+		int ShesId = 1316;
+		System.out.println("She's Id is " +   ShesId);
+		
+		char HerDivision = 'M';
+		System.out.println("Her Division is " +   HerDivision);
+		
+		int HerIdeaMobileNumber = 13022000;
+		System.out.println("Her Idea Mobile Number is " +   HerIdeaMobileNumber);
+		
+		double SheGotPercentage = 91;
+		
+	    System.out.println("she got percentage " +   SheGotPercentage+ "%");
+	
+	    
+	   // program 4
+	    
+	    byte x = 46;
+	    byte y = 87;
+	    
+	 //   byte z x+y;
+	    int z=x+y;
+	    System.out.println(z);
+	    
+	    //program 5 
+	    short m=98;
+	    short n=99;
+	 //   short o=m*n;
+	    int o=m*n;
+	    System.out.println(o);
+	    
+	    //program 6
+	    Double percentage = 75.87;
+	    
+	    System.out.println("Defined as "+ percentage + "%");
+	    
+	    float g=90.90f;
+	    float h=45.45f;
+	    float j=g/h;
+	    System.out.println(j);
+	    
+	    double xy=73.76;
+	    double yz=67.73;
+	    double xz=xy-yz;
+	    System.out.println("sagali berij vajabaki karun evde urale "+ xz + "barobar hy nhv");
+	    
+	
+	    
+	    
+	    
+	    
+	    
+	
+				
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+ }
+
